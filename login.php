501614f61fed01dd127b8c55f2028ef6c2f31082
jimytavares <slipknot_14904@hotmail.com>
2018-05-29 13:52:18 -0300

    alterações no form do login

--- login.php
+++ login.php
@@ -11,35 +11,37 @@
     
     <style>
         .error {color: #FF0000;}
-        .centro
+        body
         {
-            text-align: center;
-            padding-top: 200px;
-        }
-
-         body
-         {
             background-image: url('Imagem/wallpaper12_1280x720.jpg');
             background-color: ;
             background-repeat: no-repeat;
             background-position: left top;
-         }
+        }
+        .margin-top-form 
+        {
+            text-align: center;
+            padding-top: 100px;
+        }
+        .form-a
+        {
+            background-color: red;
+        }
     </style>
 
 </head>
 <body>  
-
-    
-    <div class="centro">        
-        <div class="container">
-
+        <div class="margin-top-form"> 
           <img src="Imagem/Mob2022.gif">
+    </div>
+    
 
-          <form method="" action="loginChampionSelect.php">
+
+          <!--<form method="" action="loginChampionSelect.php">
 
             <div class="form-group">
               <label for="user"> <h2>Usário:</h2> </label>
-              <input type="text" name="usuario" class="form-control" id="email" placeholder="Inserir Usuário">
+              <input type="text" name="usuario" class="aa" id="email" placeholder="Inserir Usuário">
             </div>
 
             <div class="form-group">
@@ -49,13 +51,47 @@
 
             <!--<div class="checkbox">
               <label><input type="checkbox"> Remember me</label>
-            </div> -->
+            </div> 
 
             <button type="submit" class="btn btn-success btn-lg">Logar</button>
 
           </form>
 
         </div>
+    </div> -->
+    
+    <!-- doasdoasldolas -->
+    <div class="margin-top-form">        
+    <div class="container">
+
+        <div class="box-parent-login">
+        <div class="well bg-white box-login">
+            <h1 class="ls-login-logo">기아보</h1>
+            <form method="" action="loginChampionSelect.php" role="form">
+                        <fieldset>
+
+                                <div class="form-group ls-login-user">
+                                    <label for="user">Usuário</label>
+                                    <input class="form-control ls-login-bg-user input-lg" id="userLogin" type="text" aria-label="Usuário" placeholder="Usuário" name="usuario">
+                                </div>
+
+                                <div class="form-group ls-login-password">
+                                    <label for="pwd">Senha</label>
+                                    <input class="form-control ls-login-bg-password input-lg" id="userPassword" type="password" aria-label="Senha" placeholder="Senha" name="senha">
+                                </div>
+
+                                <a href="#" class="ls-login-forgot">Esqueci minha senha</a>
+
+                                <input type="submit" value="Entrar" class="btn btn-primary btn-lg btn-block">
+                                <p class="txt-center ls-login-signup">Não possui um usuário na Locaweb?
+                                    <a href="#">Cadastre-se agora</a>
+                                </p>
+
+                        </fieldset>
+                    </form>
+            </div>
+            </div>
+    </div>        
     </div>
     
 </body>
